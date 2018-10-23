8e391cb7f63b398cac4f803f242786c9f334f985
Kenta Kodashima <kentakodashima@gmail.com>
2018-10-23 00:26:39 -0700

    Modified ViewController.swift. Added @IBAction version of login.

--- ViewController.swift
+++ ViewController.swift
@@ -27,6 +27,9 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     
   }
   
+  /*
+   Authentication with programatically created FBSDKLoginButton
+  */
   func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
     if let error = error {
       print(error.localizedDescription)
@@ -44,6 +47,41 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
       // ...
     }
     // ...
+  }
+  
+  /*
+   Authentication with UIButton
+  */
+  @IBAction func facebookLogin(sender: AnyObject) {
+    let LoginManager = FBSDKLoginManager()
+    LoginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
+      if let error = error {
+        print("Failed to login: \(error.localizedDescription)")
+        return
+      }
+      
+      guard let accessToken = FBSDKAccessToken.current() else {
+        print("Failed to get access token")
+        return
+      }
+      
+      let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
+      
+      // Perform login by calling Firebase APIs
+      Auth.auth().signIn(with: credential, completion: { (user, error) in
+        if let error = error {
+          print("Login error: \(error.localizedDescription)")
+          let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
+          let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+          alertController.addAction(okayAction)
+          self.present(alertController, animated: true, completion: nil)
+          
+          return
+        }
+//        self.performSegue(withIdentifier: self.signInSegue, sender: nil)
+      })
+      
+    }
   }
   
   func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
